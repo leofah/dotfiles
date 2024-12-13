6400e75081f7d15a7f06205b9879d130dae95fb9
Leo Fahrbach <fahrbach@cit.tum.de>
2024-12-13 20:19:30 +0100

    i3: xrandr shortcut to only enable the internal Laptop Screen

--- scripts/i3/lock_mons.sh
+++ scripts/i3/lock_mons.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # i3lock-color needed
+# i3lock
+
 i3lock  --force-clock --blur=5 --radius=20 --ring-width=2 \
         --ind-pos=" x + 2*r + 150 : y + h - 2*r " --time-pos=" ix - 140 : iy + 10" --date-pos=" tx : ty - 20 "  \
         --time-align=1 --date-align=1 --time-size=15 \
